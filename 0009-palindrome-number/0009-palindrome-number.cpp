aaffa2987de126e46484a943800f18a72b459f72
Anuj Shinde <anujshinde128@gmail.com>
2024-12-26 18:20:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int reverse=0;
+        int original =x;
+        while (x!=0){
+            int num = x %10; // last digit of the number!
+            reverse = reverse*10 + num;
+            x=x/10;
+        }
+        return reverse==original;
+    }
+};
