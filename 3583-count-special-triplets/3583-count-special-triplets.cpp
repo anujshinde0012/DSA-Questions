237111033200cd8f7de69ff7f8837baa8afb3500
Anuj Shinde <anujshinde128@gmail.com>
2025-12-09 19:39:05 +0530

    Time: 654 ms (66.06%), Space: 315.8 MB (88.32%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,30 +1,49 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    int specialTriplets(vector<int>& nums) {
-        unordered_map<int, int> rightCount;
-        unordered_map<int, int> leftCount;
-
-        for (int x : nums) {
-            rightCount[x]++;
+    int specialTriplets(vector<int>& nums) 
+    {
+        const int MOD = 1e9 + 7;
+        int n = nums.size();
+        
+        int max_val = *max_element(nums.begin(), nums.end());
+        int max_target = 2 * max_val;
+        
+        vector<int> left_count(max_target + 1, 0);
+        vector<int> right_count(max_target + 1, 0);
+        
+        for (int num : nums) 
+        {
+            if (num <= max_target) 
+            {
+                right_count[num]++;
+            }
         }
-
-        long long ans = 0;
-
-        for (int j = 0; j < nums.size(); j++) {
-
-            rightCount[nums[j]]--;
-
-            int target = nums[j] * 2;
-
-            long long before = leftCount[target];
-
-            long long after = rightCount[target];
-
-            ans += before * after;
-
-            leftCount[nums[j]]++;
+        
+        long long total = 0;
+        
+        for (int j = 0; j < n; j++) 
+        {
+            if (nums[j] <= max_target) 
+            {
+                right_count[nums[j]]--;
+            }
+            
+            int target = 2 * nums[j];
+            if (target <= max_target) 
+            {
+                total = (total + (long long)left_count[target] * right_count[target]) % MOD;
+            }
+            
+            if (nums[j] <= max_target) 
+            {
+                left_count[nums[j]]++;
+            }
         }
-
-        return ans;
+        
+        return total;
     }
 };
