08a02f535c094735e8f58c0ecae47c71ebb0506f
Anuj Shinde <anujshinde128@gmail.com>
2025-12-13 13:05:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonzeroes = 0;
+
+        for(int j = 0; j < nums.size(); j++)
+        {
+            if(nums[j] != nums[nonzeroes])
+            {
+                swap(nums[j], nums[nonzeroes]);
+                nonzeroes++;
+            }
+        }
+    }
+};
